147c64b3bb73dc825b8ec19a40df88ceba7b3fa4
ShirinNadaf <shirin.nadaf@successive.tech>
2021-11-09 17:35:05 +0530

    firstprogam

---
+++ basic.js
@@ -0,0 +1,34 @@
+var a=10;
+var b='javaScript';
+ //type of oprator
+console.log(a);
+console.log(typeof(b));
+
+//string type
+
+let str ="javaScript"
+console.log(str);
+
+// const type
+const c="555";
+console.log(a)
+
+// addition of two Number
+let d=10;
+let f=20;
+console.log("addition of number" ,d+f);
+
+//boolen Value
+let x =6;
+let y =9;
+let z = 22;
+let p =6;
+console.log( (x == y));
+
+//undefine
+let k;
+console.log(k);
+
+//array
+const color =['yellow' , 'black' , 'green', 'pink']
+console.log(color[2]);
